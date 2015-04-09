2b34b4266903cd7bc2fcd8c9b2800a576f6450ed
borovsky <fl.borovsky@gmail.com>
2015-04-10 00:35:38 +0200

    export sql fix

---
+++ symfony/sql_dump/dump.currency.sql
@@ -0,0 +1,66 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: currency; Type: TABLE DATA; Schema: public; Owner: fey
+--
+
+COPY currency (id, label, ratio) FROM stdin;
+USD	dolar amerykański	1
+AUD	dolar australijski	1
+HKD	dolar hongkoński	1
+CAD	dolar kanadyjski	1
+EUR	euro	1
+HUF	forint węgierski	100
+CHF	frank szwajcarski	1
+CYP	funt cypryjski	1
+GBP	funt szterling	1
+UAH	hrywna ukraińska	1
+JPY	jen japoński	100
+CZK	korona czeska	1
+DKK	korona duńska	1
+EEK	korona estońska	1
+NOK	korona norweska	1
+SKK	korona słowacka	1
+SEK	korona szwedzka	1
+MTL	lira maltańska	1
+LTL	lit litewski	1
+LVL	łat łotewski	1
+ZAR	rand południowoafrykański	1
+RUB	rubel rosyjski	1
+XDR	SDR (MFW)	1
+BGN	lew bułgarski	1
+RON	nowy lej rumuński	1
+THB	bat (Tajlandia)	1
+NZD	dolar nowozelandzki	1
+SGD	dolar singapurski	1
+ISK	korona islandzka	100
+HRK	kuna chorwacka	1
+TRY	lira turecka	1
+PHP	peso filipińskie	1
+MXN	peso meksykańskie	1
+BRL	real brazylijski	1
+MYR	ringgit malezyjski	1
+IDR	rupia (Indonezja)	10000
+KRW	won (Korea Płd.)	100
+CNY	yuan renminbi (Chiny)	1
+ILS	nowy szekel izraelski	1
+INR	rupia indyjska	100
+CLP	peso chilijskie	100
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- symfony/sql_dump/export.sh
+++ symfony/sql_dump/export.sh
@@ -1,2 +1,12 @@
-PGPASSWORD=heEGl2PVBtQ-wmMsGU2ckrwNZP psql -h ec2-107-20-159-103.compute-1.amazonaws.com -U srrceiazwsthqh d9ubjsqe1eg545 < dump.sql
+export PGPASSWORD=heEGl2PVBtQ-wmMsGU2ckrwNZP 
+c='psql -h ec2-107-20-159-103.compute-1.amazonaws.com -U srrceiazwsthqh d9ubjsqe1eg545'
+clean="$c -c 'delete from entry; delete from currency;'"
+send_currency="$c < dump.currency.sql"
+send_entry="$c <dump.entry.sql"
+echo cleaning
+eval $clean
+echo sending currency
+eval $send_currency
+echo sending entry
+eval $send_entry
 
